581340bf35ac46adbc6b2726e0c11283d4b00933
paulNasdaq <nasdaqpaul@gmail.com>
2021-04-16 17:52:30 +0300

    Schema defination v1

--- app/schemas.py
+++ app/schemas.py
@@ -18,3 +18,10 @@ class Notes:
         todos = fields.List()
         images = fields.List()
         last_update = fields.DateTime(default=datetime.datetime.utcnow())
+
+
+class User(Schema):
+    first_name = fields.String(required=True, data_key='firstName')
+    last_name = fields.String(required=True, data_key='lastName')
+    email_address = fields.Email(required=True, data_key='emailAddress')
+    password = fields.String(required=True)
